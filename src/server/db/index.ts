9bfbf733a1e9be6845214a1ec45df2ef3862d206
WiFoDev <lou81804@gmail.com>
2022-11-30 17:08:55 -0500

    add wallets db file

---
+++ src/server/db/index.ts
@@ -0,0 +1,14 @@
+export const wallets = [
+  {
+    address: "0x1",
+    balance: 100,
+  },
+  {
+    address: "0x2",
+    balance: 50,
+  },
+  {
+    address: "0x3",
+    balance: 25,
+  },
+];
--- src/server/services/getWallet.ts
+++ src/server/services/getWallet.ts
@@ -1,17 +1,4 @@
-const wallets = [
-  {
-    address: "0x1",
-    balance: 100,
-  },
-  {
-    address: "0x2",
-    balance: 50,
-  },
-  {
-    address: "0x3",
-    balance: 25,
-  },
-];
+import {wallets} from "../db";
 
 export const getWallet = (address: string) => {
   return wallets.find((wallet) => wallet.address === address);
