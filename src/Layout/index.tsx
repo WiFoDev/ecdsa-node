b52291673bf43d1af826b83bea1ea8a1705f0b93
WiFoDev <lou81804@gmail.com>
2022-11-30 15:51:56 -0500

    setup page layout

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -7,19 +7,21 @@ type LayoutProps = {
 
 export const Layout = ({children}: LayoutProps) => {
   return (
-    <>
+    <div className="min-h-screen flex flex-col justify-between">
       <Head>
-        <title>APP</title>
-        <meta content="The next generation app" name="My App" />
+        <title>ECDSA Node</title>
+        <meta
+          content="The next generation ECDSA Node"
+          name="My ECDSA Node"
+        />
         <link href="/favicon.ico" rel="icon" />
       </Head>
       <header className="sticky top-0 z-20 w-full">
         <nav className="flex pointer-events-none h-16 mx-auto max-w-screen-standar items-center justify-end gap-2 pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
-          <div className="mr-auto text-xl">My APP</div>
-          <ul className="text-lg">Navigation List For The Page</ul>
+          <div className="mr-auto text-xl">My ECDSA Node</div>
         </nav>
       </header>
-      <main className="relative flex flex-col items-center justify-center w-full h-full">
+      <main className="relative flex-1 mx-auto max-w-screen-standar pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
         {children}
       </main>
       <footer className="relative pb-[env(safe-area-inset-bottom)]">
@@ -27,6 +29,6 @@ export const Layout = ({children}: LayoutProps) => {
           Here is the footer section
         </div>
       </footer>
-    </>
+    </div>
   );
 };
